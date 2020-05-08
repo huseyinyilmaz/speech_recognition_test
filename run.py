3b550a722da6bb995d55371bbc84dce3adbc0826
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2020-05-08 08:33:53 -0700

    remove ipdb add duration check.

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-import speech_recognition as sr
+import datetime
 from os import path
 
+import speech_recognition as sr
+
+start = datetime.datetime.now()
 # file_name = '263-seo-for-python-developers.mp3'
 file_name = 'input.wav'
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), file_name)
@@ -15,9 +18,12 @@ try:
     print("Sphinx thinks you said " + result)
     with open('result.txt', 'w') as f:
         f.write(result)
-    import ipdb; ipdb.set_trace()
 
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
+
+end = datetime.datetime.now()
+duration = end - start
+print('duration = ', duration)
